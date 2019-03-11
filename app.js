20bb251d3405443f46addd5f770a52476272a894
Alex Tech <alextech1x@gmail.com>
2019-03-11 17:13:39 +0200

    1

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ bot.start((ctx) => ctx.reply(
     'Ваш язык?  |  Your language?  |  Votre langue?',
     languageKeyboard)
 )
-bot.action('rus', (ctx) => ctx.editMessageText('1Здравствуйте, Вас приветствует поддержка 1xPartners. Укажите, пожалуйста, свой email и ID (при наличии) и опишите свой вопрос:'))
+bot.action('rus', (ctx) => ctx.editMessageText('1234Здравствуйте, Вас приветствует поддержка 1xPartners. Укажите, пожалуйста, свой email и ID (при наличии) и опишите свой вопрос:'))
 bot.action('eng', (ctx) => ctx.editMessageText('Hello, welcome to 1xPartners Support. Please share with us your email and ID (if you have one) and ask your question:'))    
 bot.action('fra', (ctx) => ctx.editMessageText('Bonjour, nous sommes le service support de 1xPartners. Indiquez s’il vous plait votre adresse e-mail et votre ID (si vous le savez) et decrivez votre question ou bien votre problème:'))
 // Start Language Pick \\
